f7f87e678b9f66278788c7f9f083facac8385f85
Aruk Pupatee <aruk.pupatee@gmail.com>
2017-07-12 10:06:35 +0700

    Backup for network-shop-traffic-management #Internship #TrueAcademyProject

---
+++ app/models/users.js
@@ -0,0 +1,79 @@
+var sqlite3 = require('sqlite3').verbose();
+var bcrypt = require('bcrypt-nodejs');
+const uuidv4 = require('uuid/v4');
+
+class Users {
+  constructor(db_path){
+    this.db = new sqlite3.Database(db_path);
+  }
+  get_user_by_id(id,callback){
+    this.db.all("SELECT id,role FROM users WHERE id='"+id+"'",function(err,rows){
+      if(err) throw err;
+      if(rows.length == 0){
+        callback(null);
+      } else {
+        callback(rows[0]);
+      }
+    });
+  }
+  create_user(username,password,role,callback){
+    var Users = this;
+    Users.db.all("SELECT id FROM users WHERE username='"+username+"'",function(err,rows){
+      if(rows.length == 0){
+        var new_id = uuidv4();
+        var pass = bcrypt.hashSync(password,bcrypt.genSaltSync(8),null);
+        Users.db.run("INSERT INTO users VALUES('"+new_id+"','"+username+"','"+pass+"','"+role+"')");
+        callback("Success");
+      } else {
+        callback("User is in use");
+      }
+    });
+  }
+  authentication(username,password,callback){
+    var Users = this;
+    Users.db.all("SELECT id FROM users WHERE username='"+username+"'",function(err,rows){
+      if(rows.length > 0){
+        Users.db.all("SELECT * FROM users WHERE id='"+rows[0].id+"'",function(err,rows){
+          if(bcrypt.compareSync(password,rows[0].password)){
+            Users.get_user_by_id(rows[0].id,function(user){
+              callback(user);
+            });
+          } else {
+            callback("Wrong password");
+          }
+        });
+      } else {
+        callback("User not found");
+      }
+    });
+  }
+  change_password(id,old_password,new_password,callback){
+    var Users = this;
+    Users.db.all("SELECT * FROM users WHERE id='"+id+"'",function(err,rows){
+      if(rows.length > 0){
+        if(bcrypt.compareSync(old_password,rows[0].password)){
+          var pass = bcrypt.hashSync(new_password,bcrypt.genSaltSync(8),null);
+          Users.db.run("UPDATE users SET password='"+pass+"' WHERE id='"+id+"'");
+          callback("Success");
+        } else {
+          callback("Wrong old password");
+        }
+      } else {
+        callback("User not found");
+      }
+    });
+  }
+  get_all_users(callback){
+    var Users = this;
+    Users.db.all("SELECT id,username,role FROM users",function(err,rows){
+      callback(rows);
+    })
+  }
+  delete_user_by_id(id,callback){
+    var Users = this;
+    Users.db.run("DELETE FROM users WHERE id='"+id+"'");
+    callback("Success");
+  }
+}
+
+module.exports = Users;
---
+++ modules/zabbix-connection.js
@@ -0,0 +1,270 @@
+var request = require('request');
+
+class ZabbixConnection {
+  constructor(server_url){
+    if (!server_url) {
+      throw new Error('Server url is required');
+    }
+    this.url = server_url;
+    this.api_url = server_url+"/api_jsonrpc.php";
+    this.req_pattern = {
+      "jsonrpc": "2.0",
+      "params": null,
+      "id": null,
+      "auth": null
+    }
+  }
+  login(user, pass, callback=function(token){return token;}, id=1){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "user.login";
+    var params = {
+      "user": user,
+      "password": pass
+    };
+    req.id = id;
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      zabbix.token = data.result;
+      zabbix.id = data.id;
+      zabbix.req_pattern.auth = data.result;
+      zabbix.req_pattern.id = data.id;
+      callback(data.result);
+    });
+  }
+  logout(callback){
+    this.req_pattern.auth = null;
+    callback();
+  }
+  get_api_version(callback=function(version){return version;}, id=1){
+    var zabbix = this;
+    var req =  {
+      "jsonrpc": "2.0",
+      "method": "apiinfo.version",
+      "params": [],
+      "id": id
+    }
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  send(method,params,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = method;
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  /* get host */
+  get_all_host(callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "output": "extend",
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  get_host_by_hostids(hostids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "hostids": hostids,
+      "output": "extend",
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  get_all_host_enable(callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "output": "extend",
+      "with_monitored_items": true,
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  get_all_host_enable_by_groupid(groupids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "output": "extend",
+      "groupids": groupids,
+      "with_monitored_items": true,
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  /* get hostgroup */
+  get_all_host_group(callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "hostgroup.get";
+    var params = {
+      "output": ["groupid","name"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  /* get_item */
+  get_all_item_in_host_by_hostids(hostids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "item.get";
+    var params = {
+      "hostids": hostids,
+      "output": "extend",
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  get_item_in_host_by_itemsid(hostids,itemids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "item.get";
+    var params = {
+      "hostids": hostids,
+      "itemids": itemids,
+      "output": "extend",
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+
+  get_hosts_trigger_by_groupids(groupids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "output": ["hostid","host"],
+      "groupids": groupids,
+      "with_monitored_items": true,
+      "selectTriggers": ["description","value","priority","lastchange"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+
+  get_hosts_item_by_groupids(groupids,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "host.get";
+    var params = {
+      "output": ["hostid","host"],
+      "groupids": groupids,
+      "with_monitored_items": true,
+      "selectItems": ["name","key_","lastvalue"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+
+  get_all_map(callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "map.get";
+    var params = {
+      "output": ["sysmapid","name"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+
+  get_all_screen(callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "screen.get";
+    var params = {
+      "output": ["screenid","name"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+  get_graph_by_hostid(hostid,callback){
+    var zabbix = this;
+    var req = zabbix.req_pattern;
+    req.method = "graph.get";
+    var params = {
+      "hostids": hostid,
+      "output": ["graphid","name"],
+    }
+    req.params = params;
+    json_req(zabbix.api_url,"POST",req,function (err,res,data){
+      if (err) throw err;
+      if(data.error) throw data.error.data;
+      callback(data.result);
+    });
+  }
+}
+
+function json_req(url,method,json,callback){
+  var options = {
+    headers: {
+      name: 'content-type',
+      value: 'application/json-rpc'
+    },
+  };
+  options.url = url;
+  options.method = method;
+  options.json = json;
+  request(options,callback);
+}
+
+module.exports = ZabbixConnection;
